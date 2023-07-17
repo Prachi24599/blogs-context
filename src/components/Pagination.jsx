a7f656ed91c44df68e21d2a583c210a96c264a16
Prachi24599 <prachi24599@gmail.com>
2023-07-17 22:48:07 +0530

    pagination added

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const Pagination = () => {
-  return <div>Pagination</div>;
+  const { page, fetchBlogPosts, totalPages } = useContext(AppContext);
+
+  return (
+    <div>
+      <div>
+        {page > 1 && (
+          <button onClick={() => fetchBlogPosts(page - 1)}>Previous</button>
+        )}
+        {page < totalPages && (
+          <button onClick={() => fetchBlogPosts(page + 1)}>Next</button>
+        )}
+      </div>
+      <p>
+        Page {page} of {totalPages}
+      </p>
+    </div>
+  );
 };
 
 export default Pagination;
