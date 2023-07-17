efe81bf66b09d0da0e88152154ae9faa7485b0d0
Prachi24599 <prachi24599@gmail.com>
2023-07-17 19:47:51 +0530

    API call to fetch the data

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { baseUrl } from "../baseUrl";
 
 //Step 1 : Create Context
 export const AppContext = createContext();
@@ -9,6 +10,26 @@ function AppContextProvider({ children }) {
   const [posts, setPosts] = useState([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(null);
+
+  //fetch data
+  async function fetchBlogPosts(page = 1) {
+    setLoading(true);
+    let url = `${baseUrl}?page=${page}`;
+    try {
+      const result = await fetch(url);
+      const data = await result.json();
+      console.log(data);
+      setPage(data.page);
+      setPosts(data.posts);
+      setTotalPages(data.totalPages);
+    } catch (error) {
+      console.log("Error in fetching data");
+      setPage(1);
+      setPosts([]);
+      setTotalPages(null);
+    }
+    setLoading(false);
+  }
 
   const value = {
     loading,
