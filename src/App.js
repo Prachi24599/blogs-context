afe453fb1677f22efd8f2acdab564707a55eafaa
Prachi24599 <prachi24599@gmail.com>
2023-08-15 23:43:38 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -4,19 +4,30 @@ import Header from "./components/Header";
 import Pagination from "./components/Pagination";
 import { AppContext } from "./context/AppContext";
 import "./App.css";
+import { Route, Routes, useLocation } from "react-router";
+import { useSearchParams } from "react-router-dom";
 
 export default function App() {
   const { fetchBlogPosts } = useContext(AppContext);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
 
   useEffect(() => {
+    //fetch the initial blogposts data
     fetchBlogPosts();
   }, []);
 
   return (
-    <div className="w-full h-full flex flex-col gap-y-1 justify-center items-center">
-      <Header />
-      <Blogs />
-      <Pagination />
-    </div>
+    // <div className="w-full h-full flex flex-col gap-y-1 justify-center items-center">
+    //   <Header />
+    //   <Blogs />
+    //   <Pagination />
+    // </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/blog/:blogId" element={<BlogPage />} />
+      <Route path="/tags/:tag" element={<TagPage />} />
+      <Route path="/categories/:category" element={<CategoryPage />} />
+    </Routes>
   );
 }
