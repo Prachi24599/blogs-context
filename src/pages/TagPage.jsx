7c2e238381abf3fd6a417af00b67a5e08f636e66
Prachi24599 <prachi24599@gmail.com>
2023-08-18 12:47:17 +0530

    TagPage Created

--- src/pages/TagPage.jsx
+++ src/pages/TagPage.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Header from "../components/Header";
+import { useLocation, useNavigation } from "react-router";
+import Pagination from "../components/Pagination";
+import Blogs from "../components/Blogs";
 
 const TagPage = () => {
-  return <div>TagPage</div>;
+  const navigation = useNavigation();
+  const location = useLocation();
+  const tag = location.pathname("/").at(-1);
+  return (
+    <div>
+      <Header />
+      <div>
+        <button onClick={() => navigation(-1)}>Back</button>
+        <h2>
+          Blogs Tagged <span>${tag}</span>
+        </h2>
+      </div>
+      <Blogs />
+      <Pagination />
+    </div>
+  );
 };
 
 export default TagPage;
