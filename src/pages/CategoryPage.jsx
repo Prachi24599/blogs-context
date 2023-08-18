42c255b5cc11047d286732fb30357588bbd63972
Prachi24599 <prachi24599@gmail.com>
2023-08-18 12:49:42 +0530

    Category Page created

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import Header from "../components/Header";
+import { useLocation, useNavigation } from "react-router";
+import Pagination from "../components/Pagination";
+import Blogs from "../components/Blogs";
 
 const CategoryPage = () => {
-	return (
-		<div>CategoryPage</div>
-	)
-}
-
-export default CategoryPage
+  const navigation = useNavigation();
+  const location = useLocation();
+  const category = location.pathname("/").at(-1);
+  return (
+    <div>
+      <Header />
+      <div>
+        <button onClick={() => navigation(-1)}>Back</button>
+        <h2>
+          Blogs Tagged <span>${category}</span>
+        </h2>
+      </div>
+      <Blogs />
+      <Pagination />
+    </div>
+  );
+};
+export default CategoryPage;
