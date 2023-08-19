83f5143b8263ff1d015e953d771e3ef40a888784
Prachi24599 <prachi24599@gmail.com>
2023-08-19 21:28:11 +0530

    API Call with blog id

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useContext, useEffect } from "react";
+import { useState } from "react";
+import { useLocation, useNavigation } from "react-router";
+import { AppContext } from "../context/AppContext";
+import { baseUrl } from "../baseUrl";
 
 const BlogPage = () => {
-	return (
-		<div>BlogPage</div>
-	)
-}
+  const [blog, setBlog] = useState(null);
+  const [relatedBlogs, setRelatedBlogs] = useState([]);
+  const location = useLocation();
+  const navigation = useNavigation();
+  const { loading, setLoading } = useContext(AppContext);
+  const blogId = location.pathname.split("/").at(-1);
 
-export default BlogPage
+  async function fetchRelatedBlogs() {
+    setLoading(true);
+    let url = `${baseUrl}?blogId=${blogId}`;
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+
+      setBlog(data.blog);
+      setRelatedBlogs(data.relatedBlogs);
+    } catch (error) {
+      console.log("Error In API Call..");
+      setBlog(null);
+      setRelatedBlogs([]);
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    if (blogId) {
+      fetchRelatedBlogs();
+    }
+  }, [location.pathname]);
+
+  return <div>BlogPage</div>;
+};
+
+export default BlogPage;
